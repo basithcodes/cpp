e8b4a4a7e475f92bb5c6696a05aa4fc2726c5740
Abdul Basith <abdulbasith.eee@hotmail.com>
2024-05-10 02:20:03 +0530

    add hello from feat1

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main() {
   int rateOffStatus = 0xf0f0ffff;
   cout << std::hex << rateOffStatus << endl;
+
   list<unsigned int> tankMeterRateOffStatus;
   rateOffStatus &= 0x3fffffff;
   cout << std::hex << rateOffStatus << endl;
+  cout << "hello from feat1" << endl;
   return 0;
 }
