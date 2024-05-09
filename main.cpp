1dd8ce7fb2e1ec575562645324917f63dc49ca90
Abdul Basith <abdulbasith.eee@hotmail.com>
2024-05-10 02:31:34 +0530

    just go to the highest commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main() {
   int rateOffStatus = 0xf0f0ffff;
   cout << std::hex << rateOffStatus << endl;
-
   list<unsigned int> tankMeterRateOffStatus;
   rateOffStatus &= 0x3fffffff;
   cout << std::hex << rateOffStatus << endl;
