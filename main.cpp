73e8a1e5331f80b8c4320613179591bbfa03a1a1
Abdul Basith <abdulbasith.eee@hotmail.com>
2024-05-10 01:28:49 +0530

    remove for loop

--- main.cpp
+++ main.cpp
@@ -9,11 +9,5 @@ int main() {
   list<unsigned int> tankMeterRateOffStatus;
   rateOffStatus &= 0x3fffffff;
   cout << std::hex << rateOffStatus << endl;
-  for (int i = 0; i < 31; i = i + 2) {
-    if ((rateOffStatus >> i) & 0x3) {
-      cout << std::hex << ((rateOffStatus >> i) & 0x3) << " " << std::dec << i
-           << endl;
-    }
-  }
   return 0;
 }
